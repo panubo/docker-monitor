5dd78f614e5882bc2a3fcae24117a26ee34371ac
Andrew Cutler <macropin@gmail.com>
2017-02-13 20:40:00 +1100

    Fix mistake with socket constructor

--- register-result.py
+++ register-result.py
@@ -26,5 +26,5 @@ result['ttl'] = check_ttl
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 3030)
 sock.connect(server_address)
+sock.sendall(json.dumps(result))
 print (json.dumps(result))
-socket.sendall(json.dumps(result))
